97d82c0e4360c36ef2b76155949212dd7554c370
Vithursan Thangarasa <vithursan.thangarasa@gmail.com>
2017-08-19 13:50:28 -0400

    Add blog to AI category

--- build/purify-css.js
+++ build/purify-css.js
@@ -11,7 +11,8 @@ var content = [
     '_site/blog/index.html',
     '_site/linux/index.html',
     '_site/donate/index.html',
-    '_site/html/2017/01/31/3-steps-to-setup-website-with-Jalpc.html'
+    '_site/html/2017/01/31/3-steps-to-setup-website-with-Jalpc.html',
+    '_site/ai/2017/05/31/My-Coop-Experience-At-Scotiabank-ML-AI-Team.html'
 ]
 var css = ['static/css/style.css']
 
