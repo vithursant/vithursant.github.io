e6781b9108ea97504abfd84570c7c9c661f68a67
Vithursan Thangarasa <vithursan.thangarasa@gmail.com>
2017-09-04 14:27:57 -0400

    Test title

---
+++ js/title.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Typed.new('.title', {
+        strings: [
+            "你好.", "hola.", "안녕.", "hello."
+        ],
+        typeSpeed: 0,
+        typeSpeed: 30,
+        loop: true,
+        loopCount: null,
+        backDelay: 1000
+    });
+});
